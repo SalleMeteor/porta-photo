43a7ac5e637e701a77ede3ef7392ce78e3f47542
Levy <sumbulovich@gmail.com>
2014-04-26 09:12:47 +0000

    salas

---
+++ client/favorito/favorito.js
@@ -0,0 +1,10 @@
+/*
+Template.favorito.items = function() {
+  if (Session.equals('current_favorito', null))
+    return null;
+  else {
+    var cats = favoritos.findOne({_id:Session.get('current_favorito')});
+    if (cats && cats.items){
+      for(var i = 0; i<ctas.items.length
+*/      
+    
